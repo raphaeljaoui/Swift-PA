f613dd4252e4be45d05b1b9ae9970e9799e59bed
Sandrine Patin <yunie@MacBook-Pro-de-Sandrine.local>
2020-07-09 19:34:37 +0200

    postulate to a service

---
+++ LinkSAppIOS/Service/ApplyFactory.swift
@@ -0,0 +1,9 @@
+//
+//  ApplyFactory.swift
+//  LinkSAppIOS
+//
+//  Created by Sandrine Patin on 09/07/2020.
+//  Copyright © 2020 imane. All rights reserved.
+//
+
+import Foundation
---
+++ LinkSAppIOS/Service/ApplyWebService.swift
@@ -0,0 +1,9 @@
+//
+//  ApplyWebService.swift
+//  LinkSAppIOS
+//
+//  Created by Sandrine Patin on 09/07/2020.
+//  Copyright © 2020 imane. All rights reserved.
+//
+
+import Foundation
